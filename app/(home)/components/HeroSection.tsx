581ff48eafacaaba3784d0a74ff6c0b8fc3edf3d
miwamiwamiwa <anonimdante79@gmail.com>
2025-02-09 22:01:11 +0700

    Adding rubberband animation to the h1 element

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -1,15 +1,20 @@
-import { MovingBorderBtn } from '@/components/ui/moving.border'
+"use client";
+import { MovingBorderBtn } from '@/components/ui/moving.border';
 import Link from 'next/link'
 import React from 'react'
 import Title from './Title'
+import AnimatedLetters from './AnimatedLetters';
 
 export default function HeroSection() {
+
   return (
     <div className='min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between animate-move-up'>
       <div className='space-y-10 text-center lg:text-left'>
-        <h1 className='text-4xl lg:text-7xl font-bold'>
-            Nice to meet you! 👋 
-            <br /> <span className='underline underline-offset-8 decoration-green-500'>{"I'm Miwa."}</span>
+        <h1 
+        className='text-4xl lg:text-7xl font-bold cursor-pointer'>
+                <AnimatedLetters text="Nice to meet you! 👋" />
+                <br />
+                <AnimatedLetters text="I'm Miwa" delay={0.5} />
         </h1>
         <p className='md:w-96 text-lg text-gray-300'>
             {
@@ -34,10 +39,13 @@ export default function HeroSection() {
             </div>
             <div className='glow absolute top-[40%] right-1/2 -z-10'></div>
         </div>
-        <div className='absolute bottom-5 sm:bottom-14 left-0 sm-:left-10'>
-            <MovingBorderBtn borderRadius='0.5rem' className='p-3 font-semibold'>
-                <a>🚀 Download CV</a>
-            </MovingBorderBtn>
+        <div className="absolute bottom-5 sm:bottom-14 left-0 sm:left-10">
+          <MovingBorderBtn 
+            borderRadius="0.5rem" 
+            className="p-3 font-semibold transition-all duration-200 hover:bg-sky-400/20 hover:shadow-lg"
+          >
+            <a href="https://i.pinimg.com/736x/81/6d/30/816d307d0ba292064a9b0b64fc354c95.jpg">🚀 Download CV</a>
+          </MovingBorderBtn>
         </div>
       </div>
     </div>
