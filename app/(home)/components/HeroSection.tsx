bd5b56e5315faacd0d669123463edef3ef5445be
miwamiwamiwa <anonimdante79@gmail.com>
2025-02-14 21:01:03 +0700

    Replace Download CV link

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -44,7 +44,7 @@ export default function HeroSection() {
             borderRadius="0.5rem" 
             className="p-3 font-semibold transition-all duration-200 hover:bg-sky-400/20 hover:shadow-lg"
           >
-            <a href="https://drive.usercontent.google.com/u/0/uc?id=1FCxKHab1zFqsG_VO1MZAypOLO7TqPv1N&export=downloadhttps://drive.google.com/file/d/1FCxKHab1zFqsG_VO1MZAypOLO7TqPv1N/view">🚀 Download CV</a>
+            <a href="https://drive.google.com/file/d/1FCxKHab1zFqsG_VO1MZAypOLO7TqPv1N/view?usp=sharing">🚀 Download CV</a>
           </MovingBorderBtn>
         </div>
       </div>
