4b2eb1dbc8df217f72559fae43cd8fc1ef30f446
miwamiwamiwa <anonimdante79@gmail.com>
2025-02-18 23:07:08 +0700

    Removed unused imports and links, updated HeroSection and Navbar components

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import Navbar from './Navbar'
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { SiGithub, SiLinkedin, SiX } from "react-icons/si";
 
 export default function Footer() {
+  const socials = [
+      {
+          link: "https://www.linkedin.com/in/miwa-laksmana-anthony-851a17344/",
+          label: "Linkedin",
+          Icon: SiLinkedin,
+      },
+      {
+          link: "https://www.github.com/miwalaa",
+          label: "Github",
+          Icon: SiGithub,
+      },
+      {
+          link: "https://www.x.com/miwamiwamiwa79",
+          label: "X",
+          Icon: SiX,
+      }
+  ]
+
   return (
-    <div className='border-t mt-10'>
-      <Navbar className='flex-col gap-5' />
-    </div>
+    <nav className={cn(
+      'py-10 flex-col place-items-center border-t mt-10 animate-move-down'
+  )}>
+      <img src="../../favicon.ico" alt="M" className='h-10 mb-4' />
+      <div className='flex gap-5'>
+          {socials.map((social,index)=>{
+              const Icon = social.Icon
+
+              return <Link href={social.link} key={index} aria-label={social.label}>
+                  <Icon className='w-5 h-5 hover:scale-125 transition-all' />
+              </Link>
+          })}
+      </div>
+  </nav>
   )
 }
